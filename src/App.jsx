ace74554bca97895b486c8d75ba3ad2d1b167502
Adam Noveskey <a_noveskey@yahoo.com>
2025-01-31 17:02:13 -0500

    task - removed the BlinkText from the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,6 @@ function App() {
               width={600}
           />
       </div>
-        <div style={{ width: "100%", textAlign: "center" }}>
-            <BlinkText fontColor={"#0FFF50"} fontSize={32} isBold text={"Home Page"} />
-        </div>
         <div style={{ display: "flex", flexDirection: "row", width: "100%", textAlign: "center" }}>
             <MenuButton text={"Home"} onClick={() => console.log("Hello World!")} />
             <MenuButton text={"Bio"} onClick={() => console.log("Hello World!")} />

7274142ec208480f6fa73fc73c22e1364bae2bbc
Adam Noveskey <a_noveskey@yahoo.com>
2025-04-18 17:08:24 -0400

    task - made some tweaks to the flex styling of the album container elements

--- src/components/AlbumContainer/index.tsx
+++ src/components/AlbumContainer/index.tsx
@@ -46,22 +46,24 @@ const AlbumContainer: FC<IAlbumContainerProps> = (
         </div>
       </div>
       <div className="info">
-        <h2 className="title">
-          {title} <span className="release-type">({type})</span>
-        </h2>
-        <p className="release-date">{releaseDate}</p>
-        <p className="credits">{credits}</p>
-        <ol className="track-listing">
-          {trackList.map((track: string) => (
-            <li key={track}>{track}</li>
-          ))}
-        </ol>
-        <div className="links-in-info" data-testid="links-in-info">
-          {links.map(({ url, title }: IUrlLink, index: number) => (
-            <a key={index} href={url} rel="noopener noreferrer">
-              {title}
-            </a>
-          ))}
+        <div className="album-details">
+          <h2 className="title">
+            {title} <span className="release-type">({type})</span>
+          </h2>
+          <p className="release-date">{releaseDate}</p>
+          <p className="credits">{credits}</p>
+          <ol className="track-listing">
+            {trackList.map((track: string) => (
+              <li key={track}>{track}</li>
+            ))}
+          </ol>
+          <div className="links-in-info" data-testid="links-in-info">
+            {links.map(({ url, title }: IUrlLink, index: number) => (
+              <a key={index} href={url} rel="noopener noreferrer">
+                {title}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </div>

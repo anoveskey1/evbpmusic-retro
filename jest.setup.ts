7ed090c8d90168688d3d6aae988f690ce101002a
Adam Noveskey <a_noveskey@yahoo.com>
2025-09-11 17:43:53 -0400

    dependency update - bumped vite to 6.3.6 to address security vuln. Also removed stale console.log statement and updated the jest.setup.ts file

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,4 @@
 import { TextEncoder, TextDecoder } from "util";
 
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder as any;
+global.TextEncoder = TextEncoder as unknown as typeof global.TextEncoder;
+global.TextDecoder = TextDecoder as unknown as typeof global.TextDecoder;

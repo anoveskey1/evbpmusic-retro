577ddf3e3decf24fdb9b4287c5a36a8f6ca5fe6c
Adam Noveskey <a_noveskey@yahoo.com>
2025-09-11 16:26:09 -0400

    debugging - added hooks barrel file (index.ts), __mocks__ directory, and SlugPost test file

--- __mocks__/@sanity/image-url.js
+++ __mocks__/@sanity/image-url.js
@@ -1,7 +1,12 @@
 module.exports = function imageUrlBuilder() {
+  const chainable = {
+    width: () => chainable,
+    height: () => chainable,
+    fit: () => chainable,
+    url: () => "https://mocked.cdn/image.jpg",
+  };
+
   return {
-    image: () => ({
-      url: () => "https://mocked.cdn/image.jpg",
-    }),
+    image: () => chainable,
   };
 };

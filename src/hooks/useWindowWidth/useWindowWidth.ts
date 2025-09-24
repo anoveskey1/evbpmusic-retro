50f63f75fbbf47882139f5e19289ed9fa48aeaf6
Adam Noveskey <a_noveskey@yahoo.com>
2025-09-23 21:56:01 -0400

    fix - updated useWindowWidth to always refresh window width. Also minor style updates

--- src/hooks/useWindowWidth/useWindowWidth.ts
+++ src/hooks/useWindowWidth/useWindowWidth.ts
@@ -6,6 +6,8 @@ function useWindowWidth() {
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
 
+    handleResize(); // call once to prevent stale value on mount
+
     window.addEventListener("resize", handleResize);
 
     return () => window.removeEventListener("resize", handleResize);
